55c092f32428f6ee8c8d03e20368cf31912ca3b4
Rezos <mrpourmalaekeh@gmail.com>
2025-09-08 10:11:53 +0330

    gitignore changed

---
+++ gitLearn_Python_Projects/DAnalysis/Numpy_01.py
@@ -0,0 +1,7 @@
+import numpy as np
+array = np.array(0)
+
+print("DA extra commit np")
+
+def PrintFromNP():
+    print("from numpy modoul\n")
---
+++ gitLearn_Python_Projects/DAnalysis/Pandas_01.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+print("DA extra commit pd")
+
+def PrintFromPD():
+    print("From Pandas modouls")

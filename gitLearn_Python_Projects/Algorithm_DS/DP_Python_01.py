a7932b8a4499a950de9a1de7ebf0efe8961927b4
Rezos <mrpourmalaekeh@gmail.com>
2025-09-08 10:34:34 +0330

    undo ignored directories

---
+++ gitLearn_Python_Projects/Algorithm_DS/DP_Python_01.py
@@ -0,0 +1,8 @@
+#! this file for max of Zir Mostatil :)
+#? each Zir Mostatil specified with r1, r2, c1 and c2
+#* what is recerscive steps? what is our Dynamic Space?
+
+#test for .gitignore
+
+
+#fdsljfldfjldsjfljfdsjfdjfjs
---
+++ gitLearn_Python_Projects/Algorithm_DS/DP_Python_02.py
@@ -0,0 +1,3 @@
+#test for .gitignore!
+
+#sfhkfhsdhjiruwutwoeiprieporiq[oe[qpwovcnzcvnc,nv vcvvbskjhgskjh]]
